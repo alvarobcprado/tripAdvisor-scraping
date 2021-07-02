21b4f6311fd78394e7e94cbc6ce7e38049c310a9
Álvaro Prado <alvarob.prado@hotmail.com>
2021-07-02 13:37:45 -0400

    first commit

---
+++ hotelsInfo.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+class HotelsInfo:
+    def __init__(self):
+        self.dfColumns = ['nome', 'diaria', 'municipio', 'avaliacoes', 'data']
+        self.hotels = pd.DataFrame(columns=self.dfColumns)
+    
+    def add(self, name, value, city, reviews, date):
+        self.hotels = self.hotels.append({'nome': name, 'diaria': value, 'municipio': city, 'avaliacoes':reviews, 'data':date}, ignore_index= True)
+    
+    def finish(self):
+        self.hotels.to_excel("HoteisMT.xlsx")
+        
